6f26cf9b12c267d5f0e6ac6e713ee022c1286247
Svyatoslav Voronin <svyatoslav68@icloud.com>
2020-11-26 19:09:48 +0300

    Немного изменен тест для PersonModel

--- tests/src/test_personmodel.cpp
+++ tests/src/test_personmodel.cpp
@@ -2,12 +2,13 @@
 #include <QApplication>
 #include "cl_parametrs.hpp"
 #include "dialog_edit_file.hpp"
+#include "person_model.hpp"
 
 clParametrs appParametrs; // Параметры приложения - глобальная переменная
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
-	DialogEditTxtFile *mainDialog = new DialogEditTxtFile;
+	DialogEditTxtFile *mainDialog = new DialogEditTxtFile();
 	mainDialog->show();
 	return app.exec();
 }
